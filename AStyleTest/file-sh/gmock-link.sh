70ac13efd6834d2867440efb525e91ef51b4a18e
jimp03 <jimp03@1fe3c263-5997-42ff-936f-87a7378ef0cd>
2017-05-31 14:27:11 +0000

    Create copy of release 3.0.1

--- AStyleTest/file-sh/gmock-link.sh
+++
@@ -1,7 +0,0 @@
-# link gtest to version directory
-
-gtdir=gmock-1.7.0
-
-#link to version directory
-cd ../../
-sudo  ln  --force --symbolic --verbose  $HOME/Projects/$gtdir   $HOME/Projects/gmock
--- AStyleTest/srcx/LeakDetector.cpp
+++
@@ -1,4 +0,0 @@
-// Include this file to activate the leak detector.
-#if defined(LEAK_DETECTOR) && defined(_DEBUG) && defined(_MSC_VER)
-	#include "vld.h"
-#endif
